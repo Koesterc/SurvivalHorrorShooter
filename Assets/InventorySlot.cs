2b5577cb4812d59e34fb2706fb825aaaa2184682
Koesterc <Koesterc@gmatc.matc.edu>
2018-03-13 23:08:35 -0500

    added art and deisgned user wepaon inventor

---
+++ Assets/InventorySlot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySlot : MonoBehaviour {
+    public void Scale(int size)
+    {
+        RectTransform rt = GetComponent(typeof(RectTransform)) as RectTransform;
+        rt.sizeDelta = new Vector2(size, size);
+        foreach (Transform t in transform)
+        {
+            rt = t.GetComponent(typeof(RectTransform)) as RectTransform;
+            rt.sizeDelta = new Vector2(size-5, size-5);
+        }
+    }
+}
